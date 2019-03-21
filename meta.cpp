572d3ad66b4857bfd277f55dd123cbb331524736
Wimpy <25759224+aingwnf@users.noreply.github.com>
2019-03-21 17:15:11 +0700

    Add files via upload

---
+++ meta.cpp
@@ -0,0 +1,4 @@
+protocol = 1;
+publishedid = 1687587803;
+name = "JOS: Tentara Nasional Indonesia";
+timestamp = 5248572865914160399;
---
+++ mod.cpp
@@ -0,0 +1,7 @@
+name = "Jangkrik Old Squad";
+tooltipOwned= "Jangkrik Old Squad";
+picture = "icon.paa";
+actionName = "Website";
+action = "https://jos-gaming.com/";
+description = "MODs Tentara Nasional Indonesia by JOS Team";
+overview="MODs Tentara Nasional Indonesia by JOS Team";
